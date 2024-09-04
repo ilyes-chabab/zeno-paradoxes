452140866dd2dcc2c18c1c8dd18aa9fb046ef2ae
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-09-04 10:15:46 +0200

    transformation du code en anglais

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import pygame
 
 # suite convergeante = u de n = 1:n
 def Da_et_la_Dt(step):
-    
+    #initial distance 
     Da=2
     Dt=502
-    ecart = Dt - Da
-    temps_total=1
-    temps_par_etape =1
-    Vt= 500
+    gap = Dt - Da
+    time_total=1
+    time_by_step =1
+    #speed turtle
+    St= 500
     
     # print("#" * math.floor(Da-1) + "A" + "#"*((Dt-math.floor(Da))-1) + "T" + "#"*3)
     for i in range(step):
-        Vt=Vt/2
-        temps_total+=temps_par_etape
+        St=St/2
+        time_total+=time_by_step
         Da = Dt
-        Dt += Vt * temps_par_etape
+        Dt += St * time_by_step
         
         # print("#" * math.floor(Da) + "A" + "#"*((math.floor(Dt)-math.floor(Da))-1) + "T" + "#"*3)
-        print("temps : " ,temps_total ,"distance achille : " , Da , "distance tortue", Dt)
-        ecart = Dt - Da
-    return Da , Dt , ecart
+        # print("time : " ,time_total ,"distance achille : " , Da , "distance tortue", Dt)
+        gap = Dt - Da
+    return Da , Dt , gap
 
 def line_maker(screen,step,imageA,imageT):
     x1=80
@@ -29,12 +30,11 @@ def line_maker(screen,step,imageA,imageT):
     y=180
     n=0
     for i in range(step):
-        Da,Dt,ecart=Da_et_la_Dt(n)
+        Da,Dt,gap=Da_et_la_Dt(n)
         pygame.draw.line(screen,(0,0,0),(x1,y) ,(x2,y), 2)
-        # message(screen,20,"T",(Da,y,0,0),(0,0,0))
         screen.blit(imageA,(Da+x1,y))
         screen.blit(imageT,(Dt+x1,y))
-        message(screen,20,f"ecart : {ecart} pixels",(0,y-25),(0,0,0))
+        message(screen,20,f"gap : {gap} pixels",(0,y-25),(0,0,0))
         y+= 50
         n+=1
     
@@ -53,16 +53,17 @@ def main_achille_et_la_tortue():
     step=8
     screen = pygame.display.set_mode((1200,600))
     
-
+    #init image
     AchilleInit=pygame.image.load("image/Achille.png")
     AchilleImage=pygame.transform.scale(AchilleInit,(20,20))
     TortueInit=pygame.image.load("image/Tortue.png")
     TortueImage=pygame.transform.scale(TortueInit,(20,20))
 
+    #boucle for pygame
     while running:
         screen.fill((255,255,255))
         message(screen,40,"Paradoxe d'Achille et la Tortue",(400,0,0,0),(0,0,0))
-        message(screen,25,"temps par ligne : 1s , distance total : 1120 pixels ,vitesse initial d'Achille et de la Tortue' : 1000 et 500 pixels par seconde ",(10,100,0,0),(0,0,0))
+        message(screen,25,"time par ligne : 1s , distance total : 1120 pixels ,vitesse initial d'Achille et de la Tortue' : 1000 et 500 pixels par seconde ",(10,100,0,0),(0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running=False
@@ -71,9 +72,7 @@ def main_achille_et_la_tortue():
         if start:
             line_maker(screen,step,AchilleImage,TortueImage)
         if not start:
-            message(screen,25,"Appuyez sur n'importe quelle touche pour afficher la simulation",(350,55,0,0),(0,0,0))
-            
-             
+            message(screen,25,"Appuyez sur n'importe quelle touche du clavier pour afficher la simulation",(350,55,0,0),(0,0,0))
             
     
         pygame.display.flip()
@@ -83,6 +82,6 @@ def main_achille_et_la_tortue():
         
 
 
-# if __name__ == "__main__":
-main_achille_et_la_tortue()
-# Da_et_la_Dt(20)
+if __name__ == "__main__":
+    main_achille_et_la_tortue()
+    # Da_et_la_Dt(20)
