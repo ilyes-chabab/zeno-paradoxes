2fb46fe1162fd9739ee42453a7a9dddc5c6a1a6c
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-09-09 18:20:50 +0200

    update Dichotomy_CLI

--- Dichotomy_CLI.py
+++ Dichotomy_CLI.py
@@ -1,17 +1,30 @@
+from Dichotomy_GUI import compute_distances
+
 def paradoxDichotomy_CLI(initialDistance, tolerance):
     remainingDistance = initialDistance
     distanceTravelled = 0
     steps = 0
-    l = []
-
-    while remainingDistance > tolerance:
+    for d in compute_distances(initialDistance,tolerance):
+        print(f"\nRemaining distance: {d}")
         steps += 1
-        remainingDistance /= 2
-        distanceTravelled += remainingDistance
-        print(f"Step {steps}: Remaining distance = {remainingDistance:.10f}, Distance travelled = {distanceTravelled:.10f}")
-    print(f"\nFinal result: {remainingDistance:.10f}")
-    print(f"Steps neccesary : {steps}")
-    print(f"Total distance covered : {distanceTravelled:.10f}")
+        print(f"Steps neccesary : {steps}")
+        distanceTravelled += initialDistance
+        print(f"Total distance covered : {distanceTravelled}")
+        # print(f"Steps neccesary : {steps}")
+    # print(f"Total distance covered : {distanceTravelled:.10f}")
+        
+
+    # while remainingDistance > tolerance:
+    #     steps += 1
+    #     remainingDistance /= 2
+    #     distanceTravelled += remainingDistance
+    #     print(
+    #         f"Step {steps}: Remaining distance = {remainingDistance:.10f}, Distance travelled = {distanceTravelled:.10f}"
+    #     )
+    # print(f"\nFinal result: {remainingDistance:.10f}")
+    # print(f"Steps neccesary : {steps}")
+    # print(f"Total distance covered : {distanceTravelled:.10f}")
+
 
 # Parameters
 initialDistance = 800.0  # Total distance to cover
--- Dichotomy_GUI.py
+++ Dichotomy_GUI.py
@@ -1,6 +1,7 @@
 import pygame
 
-def paradoxDichotomy(initialDistance, tolerance):
+
+def compute_distances(initialDistance, tolerance):
     remainingDistance = initialDistance
     distanceTravelled = 0
     steps = 0
@@ -18,29 +19,27 @@ def main_dichotomy():
     pygame.init()
 
     initialDistance = 800
-    tolerance = 0.000240
+    tolerance = 0.05
 
-    surf = pygame.display.set_mode((1200,600))
+    surf = pygame.display.set_mode((1200, 600))
     surf.fill((255, 255, 255))
     tree = pygame.image.load("image/arbre-removebg-preview.png").convert_alpha()
     newTree = pygame.transform.scale(tree, (350, 300))
     rock = pygame.image.load("image/cercle.png").convert_alpha()
     newRock = pygame.transform.scale(rock, (150, 100))
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            surf.blit(newTree, (500, 150))
-            l = paradoxDichotomy(initialDistance, tolerance)
-            initialPosition = 500
-            # surf.blit(newRock, (initialPosition, 250))
-            for i in l:
-                surf.blit(newRock, (initialPosition - i, 250))
-                pygame.display.flip()
-                pygame.time.wait(1000)
-            pygame.display.flip()
-    pygame.quit()
+
+    surf.blit(newTree, (500, 150))
+    l = compute_distances(initialDistance, tolerance)
+    initialPosition = 500
+    # surf.blit(newRock, (initialPosition, 250))
+    for i in l:
+        surf.blit(newRock, (initialPosition - i, 250))
+        pygame.display.flip()
+        pygame.time.wait(500)
+    pygame.display.flip()
+
+
+pygame.quit()
 
 if __name__ == "__main__":
     main_dichotomy()
